e0d89fd99e1ed64d69d0459bac288ea76954c30e
kamlesh <sahukamleshkumar404@gmail.com>
2025-04-25 12:28:07 +0530

    Initial commit

---
+++ src/Local_Var.java
@@ -0,0 +1,8 @@
+public class Local_Var {
+    static int a =10;
+    public static void main(String[] args) {
+        //Hello obj=new Hello();
+        //System.out.println(obj.a);
+        System.out.println(Local_Var.a);
+    }
+}
