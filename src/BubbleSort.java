fbc7bfc200c7c48ff732befab3009856816b0440
kamlesh <sahukamleshkumar404@gmail.com>
2025-04-28 10:59:24 +0530

    Initial commit

---
+++ src/BubbleSort.java
@@ -0,0 +1,23 @@
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 7, 3, 19, 0, 8, 2};
+
+        bubbleSort(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
